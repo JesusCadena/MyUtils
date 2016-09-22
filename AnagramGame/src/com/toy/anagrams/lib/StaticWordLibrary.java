ce546e3d74227f8941311b61b85c85c1a4ff455c
JesusCadena <j.es.cade28@gmail.com>
2016-09-22 20:19:03 +0200

    Update StaticWordLibrary.java

--- AnagramGame/src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ AnagramGame/src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -1,33 +1,33 @@
 /*
- * Copyright (c) 2010, Oracle.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the distribution.
- *  * Neither the name of Oracle nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
- * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
- */
+Copyright (c) 2010, Oracle.
+* Tots els drets van reservar.
 
-/* Anagram Game Application */
+* Redistribució i ús dins font i formes binàries, amb o sense
+* modificació, és permitted amb la condició que les condicions següents són conegudes:
+
+* * les redistribucions de codi de font han de retenir el damunt avís de copyright,
+* aquesta llista de condicions i el següent disclaimer.
+* * Les redistribucions en forma binària han de reproduir el damunt avís
+* de copyright, aquesta llista de condicions i el següent disclaimer en
+* la documentació i/o altres materials proporcionats amb la distribució.
+* * Tampoc el nom d'Oracle ni els noms dels seus
+* col·laboradors poden soler aprovar o promoure els productes van derivar
+* d'aquest programari sense permís escrit previ específic.
+
+* AQUEST PROGRAMARI ÉS PROPORCIONAT PELS TITULARS de COPYRIGHT I COL·LABORADORS
+* "MENTRE ÉS" I QUALSSEVOL EXPRESSEN O va IMPLICAR GARANTIES, INCLOENT, PERÒ NO
+* LIMITAT A, EL va IMPLICAR GARANTIES DE MERCHANTABILITY I FITNESS PER
+* Un PROPÒSIT PARTICULAR ÉS DISCLAIMED. EN CAP ESDEVENIMENT EL COPYRIGHT 
+* OWNER O COL·LABORADORS SER PROPENS PER QUALSEVOL DIRECTE, INDIRECTE, INCIDENTAL, 
+* ESPECIAL, EXEMPLAR, O DANYS CONSEGÜENTS (INCLOENT, PERÒ NO LIMITAT 
+* A, APROVISIONAMENT DE BÉNS de SUBSTITUT O SERVEIS; PÈRDUA D'ÚS, DADA, O
+* BENEFICIS; O INTERRUPCIÓ EMPRESARIAL) TANMATEIX CAUSAT I EN QUALSEVOL TEORIA DE
+* RESPONSABILITAT, SI DINS CONTRACTE, RESPONSABILITAT ESTRICTA, O TORT (INCLOENT
+* NEGLIGÈNCIA O ALTRAMENT) SORGINT EN QUALSEVOL MANERA FORA DE L'ÚS D'AQUEST
+* PROGRAMARI, FINS I TOT SI ACONSELLAT DE LA POSSIBILITAT DE TAL DANY. 
+*/
+
+/* Anagram Aplicació de joc /
 
 package com.toy.anagrams.lib;
 
